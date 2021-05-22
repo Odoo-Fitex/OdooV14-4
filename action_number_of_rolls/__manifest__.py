7854cd9b9036b80988989b0746a67579d0b910e8
Odoo-Fitex <68330713+Odoo-Fitex@users.noreply.github.com>
2021-05-22 16:19:23 +0200

    Update __manifest__.py

--- action_number_of_rolls/__manifest__.py
+++ action_number_of_rolls/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'mrp', 'stock', 'purchase', 'bi_partial_mrp'],
+    'depends': ['base', 'sale', 'mrp', 'stock', 'purchase'],
 
     # always loaded
     'data': [
