35ea8d7f57b0e6557507a4f38fc58b4c5925fbd8
Odoo-Fitex <68330713+Odoo-Fitex@users.noreply.github.com>
2021-05-27 14:30:37 +0200

    Update __manifest__.py

--- operation_model/__manifest__.py
+++ operation_model/__manifest__.py
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "IBS",
-    'website': "http://www.ibs-odoo.com",
+    'author': "",
+    'website': "",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+#         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
